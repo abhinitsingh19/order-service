5090a3ab3219f0b0340a15f479d036525b534c05
Abhinit Singh <abhinit.kumar@siemens.com>
2020-09-19 19:00:19 +0530

    added a new order detail

--- src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
@@ -14,6 +14,7 @@ public class OrderDao {
         return Arrays.asList(new Order(101, "Mobile", 1, 30000),
                 new Order(58, "Book", 4, 2000),
                 new Order(205, "Laptop", 1, 150000),
-                new Order(809, "headset", 1, 1799));
+                new Order(809, "headset", 1, 1799),
+                new Order(800, "router", 1, 1500));
     }
 }
